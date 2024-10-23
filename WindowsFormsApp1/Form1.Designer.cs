748af475f5294dbc31d0d552abe99f82c41db76d
caesa <caesa@DESKTOP-UETU1VD>
2024-10-23 22:14:18 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBack = new System.Windows.Forms.Button();
+            this.bntForward = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.bntObr = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(24, 12);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(80, 34);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "<<";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // bntForward
+            // 
+            this.bntForward.Location = new System.Drawing.Point(110, 12);
+            this.bntForward.Name = "bntForward";
+            this.bntForward.Size = new System.Drawing.Size(80, 34);
+            this.bntForward.TabIndex = 0;
+            this.bntForward.Text = ">>";
+            this.bntForward.UseVisualStyleBackColor = true;
+            this.bntForward.Click += new System.EventHandler(this.bntForward_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(664, 20);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(80, 34);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Выбрать";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Путь:";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(236, 20);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(396, 20);
+            this.txtPath.TabIndex = 2;
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(23, 70);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(720, 368);
+            this.webBrowser.TabIndex = 3;
+            // 
+            // bntObr
+            // 
+            this.bntObr.Location = new System.Drawing.Point(571, 453);
+            this.bntObr.Name = "bntObr";
+            this.bntObr.Size = new System.Drawing.Size(135, 36);
+            this.bntObr.TabIndex = 4;
+            this.bntObr.Text = "обработать";
+            this.bntObr.UseVisualStyleBackColor = true;
+            this.bntObr.Click += new System.EventHandler(this.bntObr_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 492);
+            this.Controls.Add(this.bntObr);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.bntForward);
+            this.Controls.Add(this.btnBack);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button bntForward;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Button bntObr;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Выберите путь к папке" })
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    webBrowser.Url = new Uri(fbd.SelectedPath);
+                    txtPath.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (webBrowser.CanGoBack)
+                webBrowser.GoBack();
+        }
+
+        private void bntForward_Click(object sender, EventArgs e)
+        {
+            if (webBrowser.CanGoForward)
+                webBrowser.GoForward();
+        }
+
+        private void bntObr_Click(object sender, EventArgs e)
+        {
+            string directoryPath = $"{txtPath.Text}";
+            string[] aviFiles = Directory.GetFiles(directoryPath, "*.avi");
+
+            foreach (string aviFile in aviFiles)
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = "DivFix++.exe";
+                process.StartInfo.Arguments = $"-i \"{aviFile}\"";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+
+                process.Start();
+                process.WaitForExit();
+
+                // Optionally, you can capture the output and error messages
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+
+                // You can log or display the output and error if needed
+                Console.WriteLine(output);
+                if (!string.IsNullOrEmpty(error))
+                {
+                    MessageBox.Show("Error Input data", "ERROR",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error); ;
+                    ///Console.WriteLine("Error: " + error);
+                }
+            }
+        }
+    }
+}
